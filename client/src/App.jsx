ba672d64d47da3e2f99b6fe0424b92d775f1bcb6
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-26 17:50:21 +0545

    Setup Route for login, forgot password, and reset password pages in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,7 +37,15 @@ import { Loader } from "lucide-react";
 const App = () => {
 
   return (
-    <></>
+    <BrowserRouter>
+    <Routes>
+      {/* Auth Routes */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+    </Routes>
+    
+    </BrowserRouter>
   );
 };
 

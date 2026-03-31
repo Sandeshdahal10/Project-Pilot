873ece5f3598f8a26cdfa60e50d8e91d7d3d56a7
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-31 07:16:55 +0545

    Create updateUser function in userServices.js.

--- server/services/userServices.js
+++ server/services/userServices.js
@@ -8,3 +8,14 @@ export const createUser = async (userData) => {
     throw new Error("Error creating user: " + error.message);
   }
 };
+export const updateUser = async (id, updateData) => {
+  try {
+    const user = await User.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    }).select("-password");
+    return user;
+  } catch (error) {
+    throw new Error("Error updating user: " + error.message);
+  }
+};

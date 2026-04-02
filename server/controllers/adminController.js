358bb26eb226a6ddfae149deb21f66ff14278f1f
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-02 16:49:13 +0545

    Created the function to createTeacher in adminController.js.

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -54,3 +54,25 @@ export const deleteStudent = asyncHandler(async (req,res,next) => {
         message: "Student deleted successfully",
     });
 })
+
+export const createTeacher = asyncHandler(async (req,res,next) => {
+    const {name,email,password,department, maxStudents, expertise} = req.body;
+    if(!name || !email || !password || !department || !maxStudents || !expertise){
+        return next(new Errorhandler("Please provide all required fields",400));
+    }
+    // Here you  add logic to create a teacher in the database
+    const user = await userServices.createUser({
+        name,
+        email,
+        password,
+        department,
+        maxStudents,
+        expertise: Array.isArray(expertise) ? expertise : typeof expertise === "string" && expertise.trim() !== "" ? expertise.split(",").map(s=> s.trim()) : [],
+        role: "Teacher",
+    });
+    res.status(201).json({
+        success: true,
+        message: "Teacher created successfully",
+        user,
+    });
+});

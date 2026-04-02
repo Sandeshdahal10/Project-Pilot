3cacb5fea3f4df05c487e2df5c781dd00ff0300b
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-02 16:52:02 +0545

    Created a function deleteTeacher in adminController.js.

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -92,3 +92,19 @@ export const updateTeacher = asyncHandler(async (req,res,next) => {
         user,
     });
 })
+
+export const deleteTeacher = asyncHandler(async (req,res,next) => {
+    const {id} = req.params;
+    const user = await userServices.getUserById(id);
+    if(!user){
+        return next(new Errorhandler("Teacher not found",404));
+    }
+    if(user.role !== "Teacher"){
+        return next(new Errorhandler("User is not a teacher",400));
+    }
+    await userServices.deleteUser(id);
+    res.status(200).json({
+        success: true,
+        message: "Teacher deleted successfully",
+    });
+})

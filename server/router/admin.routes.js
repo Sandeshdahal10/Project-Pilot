3f7524cbb6ca91f4e86dd86b0132bc9d48470a32
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-02 17:01:48 +0545

    Created api for admin to crud all teacher user.

--- server/router/admin.routes.js
+++ server/router/admin.routes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import multer from "multer";
-import { createStudent, deleteStudent, updateStudent } from "../controllers/adminController.js";
+import { createStudent, createTeacher, deleteStudent, deleteTeacher, getAllUsers, updateStudent, updateTeacher } from "../controllers/adminController.js";
 import { isAuthenticated, isAuthorized } from "../middlewares/authMiddleware.js";
 const adminRouter = express.Router();
 
 adminRouter.post("/create-student", isAuthenticated, isAuthorized("Admin"), createStudent);
 adminRouter.put("/update-student/:id", isAuthenticated, isAuthorized("Admin"), updateStudent);
 adminRouter.delete("/delete-student/:id", isAuthenticated, isAuthorized("Admin"), deleteStudent);
+adminRouter.post("/create-teacher", isAuthenticated, isAuthorized("Admin"), createTeacher);
+adminRouter.put("/update-teacher/:id", isAuthenticated, isAuthorized("Admin"), updateTeacher);
+adminRouter.delete("/delete-teacher/:id", isAuthenticated, isAuthorized("Admin"), deleteTeacher);
+adminRouter.get("/users", isAuthenticated, isAuthorized("Admin"), getAllUsers);
 
 export default adminRouter;

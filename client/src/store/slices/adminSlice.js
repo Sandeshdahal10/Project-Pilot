cd59e3d10598aa7683d7d46738a78012caab3c09
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-03 07:41:00 +0545

    Created createStudent and updateStudent slice in adminSlice.js.

--- client/src/store/slices/adminSlice.js
+++ client/src/store/slices/adminSlice.js
@@ -2,7 +2,26 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
 
-
+export const createStudent = createAsyncThunk("createStudent", async(Data, thunkAPI) => {
+  try {
+    const res = await axiosInstance.post("",data);
+    toast.success(res.data.message || "Student created successfully");
+    return res.data.data.user;
+  } catch (error) {
+    toast.error(error.response?.data?.message || "Failed to create Student");
+    return thunkAPI.rejectWithValue(error.response?.data?.message);
+  }
+})
+export const updateStudent = createAsyncThunk("updateStudent", async({id,data}, thunkAPI) => {
+  try {
+    const res = await axiosInstance.put(``,data);
+    toast.success(res.data.message || "Student updated successfully");
+    return res.data.data.user;
+  } catch (error) {
+    toast.error(error.response?.data?.message || "Failed to update Student");
+    return thunkAPI.rejectWithValue(error.response?.data?.message);
+  }
+})
 const adminSlice = createSlice({
   name: "admin",
   initialState: {

42d4d0100731afa70fe6578772ace0df653d61e8
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-03 07:47:42 +0545

    Created thunkApi for deleteStudent and getAllUsers in adminSlice.js

--- client/src/store/slices/adminSlice.js
+++ client/src/store/slices/adminSlice.js
@@ -2,9 +2,9 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
 
-export const createStudent = createAsyncThunk("createStudent", async(Data, thunkAPI) => {
+export const createStudent = createAsyncThunk("createStudent", async(data, thunkAPI) => {
   try {
-    const res = await axiosInstance.post("",data);
+    const res = await axiosInstance.post("/admin/create-student",data);
     toast.success(res.data.message || "Student created successfully");
     return res.data.data.user;
   } catch (error) {
@@ -14,11 +14,30 @@ export const createStudent = createAsyncThunk("createStudent", async(Data, thunk
 })
 export const updateStudent = createAsyncThunk("updateStudent", async({id,data}, thunkAPI) => {
   try {
-    const res = await axiosInstance.put(``,data);
+    const res = await axiosInstance.put(`/admin/update-student/${id}`,data);
     toast.success(res.data.message || "Student updated successfully");
     return res.data.data.user;
   } catch (error) {
     toast.error(error.response?.data?.message || "Failed to update Student");
+    return thunkAPI.rejectWithValue(error.response?.data?.message);
+  }
+})
+export const deleteStudent = createAsyncThunk("deleteStudent", async(id, thunkAPI) => {
+  try {
+    const res = await axiosInstance.delete(`/admin/delete-student/${id}`);
+    toast.success(res.data.message || "Student deleted successfully");
+    return id;
+  } catch (error) {
+    toast.error(error.response?.data?.message || "Failed to delete Student");
+    return thunkAPI.rejectWithValue(error.response?.data?.message);
+  }
+})
+export const getAllUsers = createAsyncThunk("getAllUsers", async(id, thunkAPI) => {
+  try {
+    const res = await axiosInstance.get(`/admin/users`);
+    return res.data.data;
+  } catch (error) {
+    toast.error(error.response?.data?.message || "Failed to retreived Users");
     return thunkAPI.rejectWithValue(error.response?.data?.message);
   }
 })
@@ -34,7 +53,9 @@ const adminSlice = createSlice({
     error: null,
   },
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    
+  },
 });
 
 export default adminSlice.reducer;

22d024fdf4e5d4fbed3f3b07ce0704d0fd9d6b50
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-19 16:17:28 +0545

    Created the deadline model.

---
+++ server/models/deadline.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const deadlineSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Deadline name/title is required"],
+      trim: true,
+      maxLength: [100, "Feedback message cannot exceed 100 characters"],
+    },
+    dueDate: {
+      type: Date,
+      required: [true, "Due date is required"],
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "CreatedBy is required"],
+    },
+    Project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Indexing for better query performance
+deadlineSchema.index({ dueDate: 1, });
+deadlineSchema.index({ project: 1, });
+deadlineSchema.index({ createdBy: 1, });
+
+
+export const Deadline =
+  mongoose.models.Deadline || mongoose.model("Deadline", deadlineSchema);

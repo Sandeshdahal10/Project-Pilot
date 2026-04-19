5e8ddcd98209e79960294ac9bffae2ca36fa28cf
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-19 16:17:09 +0545

    Created the notification model.

---
+++ server/models/notification.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "user ID is required"],
+    },
+    message: {
+      type: String,
+      required: [true, "Message is required"],
+      trim: true,
+      maxLength: [1000, "Feedback message cannot exceed 1000 characters"],
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+    link: {
+      type: String,
+      default: null,
+    },
+    type: {
+      type: String,
+      enum: [
+        "request",
+        "approval",
+        "rejection",
+        "feedback",
+        "deadline",
+        "general",
+        "meeting",
+        "system",
+      ],
+      default: "general",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "low",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Indexing for better query performance
+notificationSchema.index({ user: 1,isRead: 1 });
+
+export const Notification =
+  mongoose.models.Notification ||
+  mongoose.model("Notification", notificationSchema);

e00c81d4cd8b5f7ae762d2fedca8d121ffad3d6f
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-19 16:17:46 +0545

    Created the supervisorRequest model.

---
+++ server/models/supervisorRequest.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const supervisorRequestSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Student ID is required"],
+    },
+    supervisor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Supervisor ID is required"],
+    },
+    message: {
+      type: String,
+      required: [true, "Message is required"],
+      trim: true,
+      maxLength: [250, "Message cannot exceed 250 characters"],
+    },
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "approved", "rejected"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Indexing for better query performance
+supervisorRequestSchema.index({ student: 1 });
+supervisorRequestSchema.index({ supervisor: 1 });
+supervisorRequestSchema.index({ status: 1 });
+
+export const SupervisorRequest =
+  mongoose.models.SupervisorRequest ||
+  mongoose.model("SupervisorRequest", deadlineSchema);

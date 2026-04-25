b4c7ee1ddcb85ef7f56e4f3a31415ba5b05cf46a
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-25 07:15:02 +0545

    Created createProject service function in projectServices.js to handle project creation logic. Updated getProjectByStudent to sort projects by creation date.

--- server/services/projectServices.js
+++ server/services/projectServices.js
@@ -2,3 +2,9 @@ import {Project} from "../models/project.js";
 export const getProjectByStudent = async (studentId) => {
     return await Project.findOne({student: studentId}).sort({createdAt: -1});
 }
+
+export const createProject = async (projectData) => {
+    const project = new Project(projectData);
+    await project.save();   
+    return project;
+};

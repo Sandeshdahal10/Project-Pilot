15d578ed4a0013277db9a17034c8229c543d9d07
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-25 07:35:26 +0545

    Added studentRouter with routes for student functionalities, including fetching supervisors, submitting proposals, and uploading files. Also retained admin routes for managing students and teachers in app.js.

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import errorMiddleware from "./middlewares/error.js";
 import authRouter from "./router/user.routes.js";
 import adminRouter from "./router/admin.routes.js";
+import studentRouter from "./router/student.route.js";
 
 config();
 const app = express();
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/student", studentRouter);
 app.use(errorMiddleware);
 
 export default app;

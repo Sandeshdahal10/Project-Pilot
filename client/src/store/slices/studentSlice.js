de117900f3088a43dc5e6796e82f4f6a6c880b16
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-26 15:17:45 +0545

    Fetched the api student/project-proposal in studentSlice.js.

--- client/src/store/slices/studentSlice.js
+++ client/src/store/slices/studentSlice.js
@@ -1,6 +1,24 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createAsyncThunk,
+  isRejectedWithValue,
+} from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
+
+export const submitProjectProposal = createAsyncThunk(
+  "student/submitProjectProposal",
+  async (data, thunkAPI) => {
+    try {
+      const res = await axiosInstance.post("/student/project-proposal", data);
+      toast.success("Project Proposal submitted Successfully");
+      return res.data.data?.project || res.data.data || res.data;
+    } catch (error) {
+      toast.error(error.response.data.message || "Failed to submit project proposal");
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+  },
+);
 
 const studentSlice = createSlice({
   name: "student",

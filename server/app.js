ae39a55f4468ca6331c61dbebd74ff0677367d73
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-26 15:00:33 +0545

    In app.js configured upload and temp folder.

--- server/app.js
+++ server/app.js
@@ -6,8 +6,14 @@ import errorMiddleware from "./middlewares/error.js";
 import authRouter from "./router/user.routes.js";
 import adminRouter from "./router/admin.routes.js";
 import studentRouter from "./router/student.route.js";
-
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
 config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(
@@ -17,6 +23,13 @@ app.use(
     credentials: true,
   }),
 );
+
+const uploadsDir = path.join(__dirname, "uploads");
+const tempDir = path.join(__dirname, "temp");
+
+if (!fs.existsSync(uploadsDir))
+  (fs.mkdirSync(uploadsDir), { recursive: true });
+if (!fs.existsSync(tempDir)) (fs.mkdirSync(tempDir), { recursive: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

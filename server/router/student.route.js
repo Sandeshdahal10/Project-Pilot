fd7085eac7304c419aa4e0e34d7016e50ca6c902
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-27 06:30:40 +0545

    slightly modified the student route to add a new endpoint for getting notifications for a student. Also added a new service for handling notification related logic. Additionally, made some changes to the SupervisorPage component and the studentSlice to accommodate the new notifications feature.

--- server/router/student.route.js
+++ server/router/student.route.js
@@ -1,18 +1,59 @@
-import express from 'express';
+import express from "express";
 import multer from "multer";
-import { getAvailableSupervisors,getStudentProject,submitProposal,uploadFiles } from "../controllers/studentController.js";
-import { isAuthenticated, isAuthorized } from "../middlewares/authMiddleware.js";
+import {
+  getAvailableSupervisors,
+  getStudentProject,
+  getSupervisor,
+  requestSupervisor,
+  submitProposal,
+  uploadFiles,
+} from "../controllers/studentController.js";
+import {
+  isAuthenticated,
+  isAuthorized,
+} from "../middlewares/authMiddleware.js";
 import { upload, handleUploadError } from "../middlewares/upload.js";
 const studentRouter = express.Router();
 
-studentRouter.post("/project", isAuthenticated, isAuthorized("Student"), getStudentProject);
-studentRouter.post("/project-proposal", isAuthenticated, isAuthorized("Student"), submitProposal);
+studentRouter.post(
+  "/project",
+  isAuthenticated,
+  isAuthorized("Student"),
+  getStudentProject,
+);
+studentRouter.post(
+  "/project-proposal",
+  isAuthenticated,
+  isAuthorized("Student"),
+  submitProposal,
+);
 
-studentRouter.post("/upload/:projectId", isAuthenticated, isAuthorized("Student"), 
-upload.array("files",10),
-handleUploadError, 
-uploadFiles);
+studentRouter.post(
+  "/upload/:projectId",
+  isAuthenticated,
+  isAuthorized("Student"),
+  upload.array("files", 10),
+  handleUploadError,
+  uploadFiles,
+);
 
-studentRouter.get("/fetch-supervisors", isAuthenticated, isAuthorized("Student"), getAvailableSupervisors);
+studentRouter.get(
+  "/fetch-supervisors",
+  isAuthenticated,
+  isAuthorized("Student"),
+  getAvailableSupervisors,
+);
+studentRouter.get(
+  "/supervisor",
+  isAuthenticated,
+  isAuthorized("Student"),
+  getSupervisor,
+);
+studentRouter.post(
+  "/request-supervisor",
+  isAuthenticated,
+  isAuthorized("Student"),
+  requestSupervisor,
+);
 
 export default studentRouter;
